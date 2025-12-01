cf28002396b67d40c525905e38fc77afbbc0516b
Jiranu-Fok <67160124@go.buu.ac.th>
2025-12-01 17:16:34 +0700

    เซฟ
    เซฟ

--- resources/views/html101.blade.php
+++ resources/views/html101.blade.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ทดสอบ
-</body>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Workshop HTML</title>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+    </head>
+
+
+    <body>
+        <div class="container pt-5">
+            <h1>Workshop #HTML - FORM</h1>
+            <form>
+                <div class="row align-items-center">
+<div class="col-4">
+    <label for="fName" class="col-from-label">
+        ชื่อ
+    </label>
+</div>
+<div class="col-6"></div>
+            <input type="text" id="fName" class="form-control"
+class="col-auto">
+<span id="passwordHelpInline" class="form-text">
+</span>
+</div>
+
+
+<div class="col-4">
+    <label for="lName" class="col-form-label">
+        นามสกุล
+    </label>
+</div>
+<div class="col-6">
+    <input type="text" id="lName" class="form-control"
+
+class="col-auto">
+
+</div>
+    </body>
 </html>
